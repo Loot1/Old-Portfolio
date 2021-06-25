47ebe7705ecbcf1fd916605c7db244bc473f4c21
Louis Sachy <louisachy@icloud.com>
2021-06-25 11:35:18 +0200

    Modify scripts

--- js/navigation.js
+++ js/navigation.js
@@ -1,12 +1,12 @@
-let mainNavLinks = document.querySelectorAll("nav ul li a");
+let mainNavLinks = document.querySelectorAll("nav ul li a")
 
 mainNavLinks.forEach(link => {
   link.addEventListener("click", event => {
-    event.preventDefault();
-    let target = document.querySelector(event.target.hash);
+    event.preventDefault()
+    let target = document.querySelector(event.target.hash)
     target.scrollIntoView({
       behavior: "smooth",
       block: "start"
-    });
-  });
-}); 
+    })
+  })
+})
--- js/timeline.js
+++ js/timeline.js
@@ -1,24 +1,24 @@
 $(function ($) {
-    var timelineBlocks = $('.cd-timeline-block');
-    var offset = 0.8;
-    hideBlocks(timelineBlocks, offset);
+    var timelineBlocks = $('.cd-timeline-block')
+    var offset = 0.8
+    hideBlocks(timelineBlocks, offset)
     $(window).on('scroll', function () {
         (!window.requestAnimationFrame)
-                ? setTimeout(function () {
-                    showBlocks(timelineBlocks, offset);
-                }, 100)
-                : window.requestAnimationFrame(function () {
-                    showBlocks(timelineBlocks, offset);
-                });
-    });
+            ? setTimeout(function () {
+                showBlocks(timelineBlocks, offset)
+            }, 100)
+            : window.requestAnimationFrame(function () {
+                showBlocks(timelineBlocks, offset)
+            })
+    })
     function hideBlocks(blocks, offset) {
         blocks.each(function () {
-            ($(this).offset().top > $(window).scrollTop() + $(window).height() * offset) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
-        });
+            ($(this).offset().top > $(window).scrollTop() + $(window).height() * offset) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden')
+        })
     }
     function showBlocks(blocks, offset) {
         blocks.each(function () {
-            ($(this).offset().top <= $(window).scrollTop() + $(window).height() * offset && $(this).find('.cd-timeline-img').hasClass('is-hidden')) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
-        });
+            ($(this).offset().top <= $(window).scrollTop() + $(window).height() * offset && $(this).find('.cd-timeline-img').hasClass('is-hidden')) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in')
+        })
     }
-});
+})
