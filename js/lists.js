8c6ef2220610824b0e27f79aa23308c2e392478d
Louis Sachy <louisachy@icloud.com>
2021-09-17 14:05:08 +0200

    Update skills and add a system to manage the skills, that's now more easy to update skills in the lists.js file

---
+++ js/lists.js
@@ -0,0 +1,42 @@
+const levelnames = ["Débutant","Intermédiaire","Avancé","Maîtrise"]
+const colornames = ["danger","warning","info","success"]
+const categorynames = ["languages", "frameworks", "tools"]
+
+const skills = {
+    languages: [
+        {title:"HTML", level:3},
+        {title:"CSS", level:2},
+        {title:"Java", level:1},
+        {title:"JavaScript", level:3},
+        {title:"Python", level:1},
+        {title:"Shell", level:1},
+        {title:"Dart", level:0}
+    ],
+    frameworks: [
+        {title:"DiscordJS", level:3},
+        {title:"NodeJS", level:2},
+        {title:"React-Native", level:3},
+        {title:"React", level:1},
+        {title:"Flutter", level:0}
+    ],
+    tools: [
+        {title:"Git", color:"success", level:3},
+        {title:"Visual Studio Code", color:"success", level:3},
+        {title:"Android Studio", color:"success", level:2}
+    ]
+}
+
+categorynames.forEach(element => {
+    skills[element.toString()].forEach((data) => {
+        document.getElementById(element).innerHTML += `
+        <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        ${data.title}<br/>
+        <div class="progress md-progress my-2" style="height: 16px; border-radius:20px">
+            <div class="progress-bar bg-${colornames[data.level]}" role="progressbar" style="width: 100%">
+                ${levelnames[data.level]}
+            </div>
+            </div>
+        </div>
+        `
+    })
+})
